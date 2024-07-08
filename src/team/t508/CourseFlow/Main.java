333761c8e3a7997d13bd1144004d6e625ec0c00d
20221113215@stu.sspu.edu.cn <128081196+William-Zack@users.noreply.github.com>
2024-07-08 22:43:19 +0800

    构建简单的项目结构

---
+++ src/team/t508/CourseFlow/Main.java
@@ -0,0 +1,11 @@
+package team.t508.CourseFlow;
+
+/**
+ * @author William-Zack
+ */
+public class Main {
+    public static void main(String[] args) {
+        // 创建项目结构，此语句作为占位用，应该在后续开发中被更改为菜单语句
+        System.out.println("Welcome to CourseFlow!");
+    }
+}
---
+++ src/team/t508/CourseFlow/utils/Utility.java
@@ -0,0 +1,11 @@
+package team.t508.CourseFlow.utils;
+
+/**
+ * @author William-Zack
+ */
+public class Utility {
+    // 创建项目结构，此语句作为占位用，应该在后续开发中被更改为其他语句
+    public static String getWelcomeMessage() {
+        return "Welcome to CourseFlow Utility!";
+    }
+}
